13f33a7a9a4c2a639aec684aaef5b6770a6ad1a4
Ferdi Şimşek <ferdi.simsek@adisyo.com>
2025-02-16 21:01:10 +0300

    Sistem duzenlemeleri

--- ProductApi/obj/Debug/net9.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net9.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da9a1cc1708c58b2227162d3b6aa9dd03f63c3cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a6102b6fb17d598b0bb33422fb5ba06620dad51")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
